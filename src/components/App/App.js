ac1616503b93dd4ebe85020ea5d5e567136c5aee
Ryan Maxie <MichaelRMaxie@Gmail.com>
2023-08-12 23:26:04 -0500

    Added assets. Created start menu.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,19 @@
+import { StartMenu } from 'components';
+import { useState } from 'react';
 import styles from './styles.module.css';
 
 export const App = () => {
-  return <div className={styles.main}>App</div>;
+  const [mode, setMode] = useState('start');
+
+  return (
+    <div className={styles.main}>
+      {mode === 'start' && (
+        <StartMenu onStartClick={() => setMode('battle')} />
+      )}
+
+      {mode === 'battle' && <>Battle Mode</>}
+
+      {mode === 'gameOver' && <>Game Over</>}
+    </div>
+  );
 };
